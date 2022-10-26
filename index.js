e9dbee06ab4973aab395279250d1a4194d9d7ea6
cse-mominul <cse.mominul.islam>
2022-10-26 19:56:57 +0600

    Update

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app =express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
-
+const course = require('./courses.json')
 const categories = require('./category.json')
 app.get('/categories',(req,res)=>{
     res.send(categories)
 })
-app.get('/',(req, res)=>{
+app.get('/course/:id',(req,res)=>{
+    const id = req.params._id;
+    const selectedCourse = course.find(n => n.id === id);
+    res.send(selectedCourse);
 
+})
+app.get('/',(req, res)=>{
     res.send('Api Runing')
 })
 
